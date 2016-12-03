871841d4a3b524afa90ed3687da7027df6c54383
西集 <ywfwj2008@163.com>
2016-12-03 16:36:45 +0800

    update

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+[ -z "`grep ^'export PATH=' /etc/profile`" ] && echo "export PATH=$WEBSERVER_INSTALL_DIR/sbin:\$PATH" >> /etc/profile
+[ -n "`grep ^'export PATH=' /etc/profile`" -a -z "`grep $WEBSERVER_INSTALL_DIR /etc/profile`" ] && sed -i "s@^export PATH=\(.*\)@export PATH=$WEBSERVER_INSTALL_DIR/sbin:\1@" /etc/profile
+
+sed -i "s@/usr/local/nginx@$WEBSERVER_INSTALL_DIR@g" /etc/init.d/nginx && \
+chmod +x /etc/init.d/nginx && \
+ldconfig
